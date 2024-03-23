1f91a7791cb49290cd90543c0427d228b0a7973a
Ben Moussa Chaker <chaker.info@gmail.com>
2024-03-24 00:56:16 +0100

    use multer

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -6,6 +6,21 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const path = require("path")
 const fs = require("fs")
+const multer = require("multer")
+// Configuration de multer pour stocker les fichiers dans le dossier "users_files"
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", "users_files")) // Chemin du dossier de destination
+  },
+  filename: function (req, file, cb) {
+    const orderId = req.body.orderId // Identifiant de la commande
+    const timestamp = Date.now() // Timestamp actuel
+    const fileName = `photo_${orderId}_${timestamp}.${
+      file.mimetype.split("/")[1]
+    }` // Nom du fichier
+    cb(null, fileName)
+  },
+})
 exports.login = async (req, res) => {
   try {
     const { username, password } = req.body
@@ -50,10 +65,11 @@ exports.getOrder = async (req, res) => {
     })
   }
 }
+const upload = multer({ storage: storage })
 
 exports.uploadPhoto = async (req, res) => {
   try {
-    // Récupérer les données de l'image depuis la requête
+    /* // Récupérer les données de l'image depuis la requête
     const imageData = req.body.imageData
     const orderId = req.body.orderId
 
@@ -73,6 +89,48 @@ exports.uploadPhoto = async (req, res) => {
     const order = await Order.findByIdAndUpdate(orderId, {
       urlPicture: photoName,
     })
+    res.status(200).json({ imageUrl })*/
+    // Utilisez multer pour gérer le téléchargement du fichier
+    /* upload.single("image")(req, res, async (err) => {
+      if (err instanceof multer.MulterError) {
+        // Gérer les erreurs multer
+        return res.status(500).json({
+          message: "Une erreur est survenue lors du téléchargement du fichier.",
+        })
+      } else if (err) {
+        // Gérer les autres erreurs
+        return res.status(500).json({
+          message: "Une erreur est survenue lors du téléchargement du fichier.",
+        })
+      }
+      console.log("req.body", req.body)
+      console.log("req.file", req.file)
+      // Le fichier a été téléchargé avec succès
+      const orderId = req.body.orderId
+      const fileName = req.file.filename // Nom du fichier téléchargé
+
+      // Enregistrez l'URL de l'image dans votre base de données ou effectuez d'autres opérations nécessaires
+
+      // Retournez l'URL de l'image téléchargée en tant que réponse
+      const imageUrl = `${process.env.URL_CLIENT}/api/admin/picture/${orderId}`
+      const order = await Order.findByIdAndUpdate(orderId, {
+        urlPicture: fileName,
+      })
+      res.status(200).json({ imageUrl })
+    })*/
+    if (!req.file) {
+      return res.status(400).json({ message: "Aucun fichier téléchargé" })
+    }
+    const orderId = req.body.orderId
+    // Obtenir le nom du fichier téléchargé
+    const fileName = req.file.filename
+    // Construire le chemin du fichier téléchargé
+    const filePath = path.join(__dirname, "..", "users_files", fileName)
+
+    // Récupérer l'URL de l'image enregistrée
+    const imageUrl = `${process.env.URL_CLIENT}/api/admin/picture/${orderId}`
+    // Mettre à jour l'ordre avec l'URL de l'image
+    await Order.findByIdAndUpdate(orderId, { urlPicture: fileName })
     res.status(200).json({ imageUrl })
   } catch (error) {
     console.error("Erreur lors de l'enregistrement de la photo :", error)
@@ -89,10 +147,28 @@ module.exports.getUserPictures = async (req, res) => {
       const filePath = path.resolve(__dirname, "..", "users_files")
       const file = filePath + "/" + order?.urlPicture
       const exists = require("fs").existsSync(file)
+      console.log("file", file, exists)
       if (exists) {
+        const ext = path.extname(file)
+        let contentType = ""
+        switch (ext) {
+          case ".jpg":
+          case ".jpeg":
+            contentType = "image/jpeg"
+            break
+          case ".png":
+            contentType = "image/png"
+            break
+          case ".gif":
+            contentType = "image/gif"
+            break
+          default:
+            contentType = "application/octet-stream"
+        }
+
         const imageContent = require("fs").readFileSync(file)
         // Envoyer le contenu de l'image en réponse avec le type de contenu approprié
-        res.writeHead(200, { "Content-Type": "image/jpeg" })
+        res.writeHead(200, { "Content-Type": contentType })
         res.end(imageContent, "binary")
       } else {
         res.status(404).send("Picture not found")
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,11 +1,31 @@
 const express = require("express")
 const router = express.Router()
 const adminController = require("../controllers/adminController")
+const multer = require("multer")
+const path = require("path")
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "users_files/")
+  },
+  filename: function (req, file, cb) {
+    // Obtenir l'extension du fichier d'origine
+    const ext = path.extname(file.originalname)
 
+    // Générer un nom de fichier unique avec un horodatage
+    cb(null, `${file.fieldname}-${Date.now()}${ext}`)
+  },
+})
+
+const upload = multer({ storage: storage })
+
+router.post(
+  "/uploadPhoto",
+  upload.single("imageData"),
+  adminController.uploadPhoto
+)
 // Routes
 router.post("/login", adminController.login)
 router.get("/requests", adminController.getOrder)
-router.post("/uploadPhoto", adminController.uploadPhoto) // Nouvel endpoint pour le téléchargement de photos
 router.get("/picture/:pictureId", adminController.getUserPictures)
 
 module.exports = router
